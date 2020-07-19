beccc54e573b3ce1968f0672c630bf607516c193
Björn Wilmsmann <bjoern@wilmsmann.de>
2020-07-19 16:11:30 +0200

    DB invoice display

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const puppeteer = require('puppeteer');
+
+if(process.argv.length < 4) {
+    console.log('Please provide username and password as arguments.');
+    process.exit(0);
+}
+
+(async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+	const navigationPromise = page.waitForNavigation();
+
+	await page.goto('https://fahrkarten.bahn.de/privatkunde/start/start.post?lang=de&country=DEU&scope=bahnbrs&dbkanal_007=L01_S01_D001_KIN0001_top-navi-flyout_meinebahn-uebersicht_LZ01');
+	
+	await page.setViewport({ width: 1272, height: 1018 });
+
+	await page.type('#login-input-loginname', process.argv[2]);
+	await page.type('#password', process.argv[3]);
+	await page.click('.button-abschnitt button');
+	await page.waitForNavigation();
+
+	const cookies = await page.cookies();
+
+	await page.click('#content > div.maincontentwhitebgbborder > div.brsoverview > table > tbody > tr:nth-child(1) > td:nth-child(6) > a');
+	await page.waitForNavigation();
+	
+	await page.click('.button-abschnitt button');
+	await page.waitForNavigation();
+
+	await navigationPromise;
+
+	await browser.close();
+
+	process.exit(0);
+})();
